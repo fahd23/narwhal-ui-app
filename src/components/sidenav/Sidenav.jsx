b5a14e9bdbd25aeaceceb0c06962de216fd417b6
Fahd <fahdpno@gmail.com>
2022-09-02 09:16:13 +0530

    REFACTOR: Refactored code and added jsconfig file

--- src/components/sidenav/Sidenav.jsx
+++ src/components/sidenav/Sidenav.jsx
@@ -1,18 +1,25 @@
 import React from "react";
-import { PathLogo,MenuCampaigns,MenuContacts,MenuReports,MenuHelp,MenuTeams } from "./Shared";
+import {
+  PathLogo,
+  MenuCampaigns,
+  MenuContacts,
+  MenuReports,
+  MenuHelp,
+  MenuTeams,
+} from "./Shared";
 import "./sidenav.css";
 
 const Sidenav = () => {
   return (
     <div className="sidenav">
-      <PathLogo/>
-      <MenuCampaigns/>
-      <MenuTeams/>
-      <MenuContacts/>
-      <MenuReports/>
-      <MenuHelp/>
+      <PathLogo />
+      <MenuCampaigns />
+      <MenuTeams />
+      <MenuContacts />
+      <MenuReports />
+      <MenuHelp />
     </div>
   );
 };
 
-export { Sidenav };
+export default Sidenav;
